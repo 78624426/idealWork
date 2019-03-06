df90ec775b33ffe3583eef8ee90342d0e99e4ca8
78624426 <78624426@qq.com>
2019-03-06 10:50:59 +0800

    Initial commit

---
+++ src/day1/Hello.java
@@ -0,0 +1,15 @@
+package day1;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Hello {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("hello world");
+        FileOutputStream f = new FileOutputStream("sss");
+        FileInputStream fis;
+
+    }
+}
